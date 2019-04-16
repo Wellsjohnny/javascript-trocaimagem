bc5c4cd535a516631c5b7d3112bd0431d3718fbb
Wellsjohnny <wmsswizard@gmail.com>
2019-04-15 21:35:12 -0300

    aulaJavaScriptTrocaImagem

---
+++ js/funcoes.js
@@ -0,0 +1,7 @@
+function trocaImagem () {
+	document.getElementById("myImage").src="img/html-logo-badge-512.png"; 
+}
+
+function Imagemtroca () {
+	document.getElementById("myImage").src="img/js-logo-badge-512.png"; 
+}
